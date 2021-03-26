0fb56531e0c2d2d6ebab71022b66d4bfa73a41af
Audrius Rudalevicius <audrius.rudalevicius@hometogo.com>
2021-03-26 17:11:39 +0200

    init

--- basics1/background.sh
+++ basics1/background.sh
@@ -6,4 +6,6 @@ docker run -d -p 5000:5000 --restart=always --name registry registry:2
 #     --from-file=.dockerconfigjson=/root/config.json \
 #     --type=kubernetes.io/dockerconfigjson
 
-#echo 'done' > /opt/katacoda-background-finished
+minikube start
+
+echo 'done' > /opt/katacoda-background-finished
