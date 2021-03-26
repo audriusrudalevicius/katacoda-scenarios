3e7b1d1723973f061920f5098ce11eb6e1eb6893
Audrius Rudalevicius <audrius.rudalevicius@hometogo.com>
2021-03-26 16:38:57 +0200

    init

--- basics1/background.sh
+++ basics1/background.sh
@@ -5,6 +5,10 @@ mkdir /root/tot
 cd /root/tot
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
+echo "Starting Katacoda... This may take a few moments"
+
+while [ ! -f /usr/local/bin/wait.sh ]; do sleep 1; done; sleep 1;  /usr/local/bin/wait.sh; /usr/local/bin/start.sh
+
 kubectl create secret generic regcred \
     --from-file=.dockerconfigjson=/root/config.json \
     --type=kubernetes.io/dockerconfigjson
