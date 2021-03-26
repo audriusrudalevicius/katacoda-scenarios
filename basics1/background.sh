5a9f5179106029a18a4b9df7f14d44bce4c9435b
Audrius Rudalevicius <audrius.rudalevicius@hometogo.com>
2021-03-26 16:30:00 +0200

    init

---
+++ basics1/background.sh
@@ -0,0 +1,12 @@
+set -euo pipefail
+IFS=$'\n\t'
+
+mkdir /root/tot
+cd /root/tot
+docker run -d -p 5000:5000 --restart=always --name registry registry:2
+
+kubectl create secret generic regcred \
+    --from-file=.dockerconfigjson=/root/config.json \
+    --type=kubernetes.io/dockerconfigjson
+
+echo 'done' > /opt/katacoda-background-finished
